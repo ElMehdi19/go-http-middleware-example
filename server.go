f28bff36e39c8d2498bc85e11e7b7f1f3ddddaea
El Mehdi Rami <lbiggboss19@gmail.com>
2021-03-25 20:53:53 +0100

    simple middlware example

--- server.go
+++ server.go
@@ -6,12 +6,24 @@ import (
 	"net/http"
 )
 
+func logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Request received")
+		/*
+			any code written here will be executed before
+			passing the request object to the next middleware
+		*/
+		next.ServeHTTP(w, r)
+		log.Println("Response sent")
+	}
+}
+
 func main() {
 	port := 5000
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", logger(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello Gophers!\n")
-	})
+	}))
 
 	log.Printf("Running on http://127.0.0.1:%d 🚀🚀", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
