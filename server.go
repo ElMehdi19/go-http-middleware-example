eacbe61389c9d64c16a2d8069cf2e4a7df267ee4
El Mehdi Rami <lbiggboss19@gmail.com>
2021-03-25 21:53:02 +0100

    content-type middlware implemented

--- server.go
+++ server.go
@@ -21,12 +21,25 @@ func logger(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func isJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		contentType := r.Header.Get("Content-Type")
+		if contentType != "application/json" {
+			http.Error(w, "content-type header must be application/json", http.StatusBadRequest)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Howdy Gophers 👋\n")
+}
+
 func main() {
 	port := 5000
 
-	http.HandleFunc("/", logger(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello Gophers!\n")
-	}))
+	http.HandleFunc("/", logger(isJSON(home)))
 
 	log.Printf("Running on http://127.0.0.1:%d 🚀🚀", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
